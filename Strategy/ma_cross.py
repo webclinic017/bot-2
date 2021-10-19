03b93714d95b125b9894d3d2505d376b46da7abf
itsbeeut <91843496+itsbeeut@users.noreply.github.com>
2021-10-19 19:49:20 +0000

    Update ma_cross.py

--- Strategy/ma_cross.py
+++ Strategy/ma_cross.py
@@ -1,9 +1,25 @@
+import pandas_ta as ta
+# last = iloc[-1]  [-2]  [-3] ...
+
 def strategy(df):
-    close = df['close']
-    open = df['open']
+    time = df[1]
+    open = df[4]
+    high = df[5]
+    low = df[6]
+    close = df[7]
+    volume = df[8]
+    
+    rsi = ta.rsi(close)
+    ema = ta.ema(close)
+
+    if rsi.iloc[-1] >= 70:
+        print("Its time to sell")
+    
+    if rsi.iloc[-1] <= 30:
+        print("Its time to buy")
 
 def tpsl():
-    print("take profit, stop loss")
+    print("Take Profit/Stop Loss")
 
 def test():
-    print("TEST HERE")
+    print("Strategy TESTER Here")
