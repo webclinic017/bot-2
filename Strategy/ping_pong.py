0f376c9d2bc549b9beba7f6ec479f60e462bd405
itsbeeut <91843496+itsbeeut@users.noreply.github.com>
2021-10-27 00:24:23 +0000

    Create ping_pong.py

---
+++ Strategy/ping_pong.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pandas_ta as ta
+
+def strategy(df):
+    # print(df)
+    time = df[0]
+    open = df[1]
+    high = df[2]
+    low = df[3]
+    close = df[4]
+    volume = df[5]
+
+    # 
+
+
+def tp(df, side):
+    print("TEST")
+
+def test():
+    print("TEST HERE")
