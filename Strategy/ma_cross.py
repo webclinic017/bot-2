32318c9d36210323d6787121e8b9796fc90a51c6
itsbeeut <91843496+itsbeeut@users.noreply.github.com>
2021-10-27 00:23:48 +0000

    Update ma_cross.py

--- Strategy/ma_cross.py
+++ Strategy/ma_cross.py
@@ -1,25 +1,43 @@
+import pandas as pd
 import pandas_ta as ta
+
 # last = iloc[-1]  [-2]  [-3] ...
 
 def strategy(df):
-    time = df[1]
-    open = df[4]
-    high = df[5]
-    low = df[6]
-    close = df[7]
-    volume = df[8]
-    
+    # print(df)
+    time = df[0]
+    open = df[1]
+    high = df[2]
+    low = df[3]
+    close = df[4]
+    volume = df[5]
+
     rsi = ta.rsi(close)
-    ema = ta.ema(close)
-
-    if rsi.iloc[-1] >= 70:
-        print("Its time to sell")
+    ema_5 = ta.ema(close,length=5)
+    ema_8 = ta.ema(close,length=8)
+    ema_18 = ta.ema(close,length=18)
+    atr    = ta.atr(high,low,close)
+    cat = pd.concat([df, rsi, ema_5, ema_8, ema_18, atr], axis=1)
     
-    if rsi.iloc[-1] <= 30:
-        print("Its time to buy")
+    print(cat)
 
-def tpsl():
-    print("Take Profit/Stop Loss")
+    last_close = cat[4].iloc[-1]
+    tp = last_close * 1.003
+    sl = last_close * 0.997
+
+    return ["buy", last_close, sl, tp]
+    
+
+def tp(df, side):
+    rsi = ta.rsi(df[4]).iloc[-1]
+
+    if side == "BUY":
+        if rsi == 30:
+            return True
+    
+    if side == "SELL":
+        if rsi == 70:
+            return True
 
 def test():
-    print("Strategy TESTER Here")
+    print("TEST HERE")
